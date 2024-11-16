aeb42115249dd8be35d236ce4066b20a64dd35c8
elwyen <demianimrehun@gmail.com>
2024-11-16 12:01:37 +0100

    addproduct button created

--- src/pages/adminView.js
+++ src/pages/adminView.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '../admin/adminComponents/table.js';  // Import Table component
 import { columns, data } from '../admin/adminComponents/tableData.js';
 import AddProductForm from '../admin/adminComponents/addProductForm.js';
+import { Button } from 'react-bootstrap';
 
 const AdminView = () => {
+
+    // State to toggle the visibility of the AddProductForm
+    const [showAddProductForm, setShowAddProductForm] = useState(false);
+  
+    // Function to toggle the form visibility
+    const toggleAddProductForm = () => {
+      setShowAddProductForm((prevState) => !prevState);
+    };
+
   return (
     <div>
       <h1>Admin Page</h1>
@@ -11,9 +21,16 @@ const AdminView = () => {
           <article>
           <h1>Admin Dashboard</h1>
           <p>This is the admin dashboard.</p>
+
+        <Button variant="primary" onClick={toggleAddProductForm}>
+          {showAddProductForm ? "Cancel" : "Add New Item"}
+        </Button>
+
+        {showAddProductForm && (
           <div>
             <AddProductForm />
           </div>
+        )}
           <div>
         <Table columns={columns} data={data}/> 
       </div>
